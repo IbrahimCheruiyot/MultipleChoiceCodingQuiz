a1628fd9715e415aafd0338408b3e96b7469840a
IbrahimCheruiyot <Ibrahimcheruiyot96@gmail.com>
2022-11-23 15:28:40 +0300

    Code revamp

--- assets/script.js
+++ assets/script.js
@@ -56,31 +56,31 @@ const viewScrBtn = document.querySelector("#view-scores");
 const questions = [ // array of objects
     {
 
-        // question 0
+        // 1st question
         question: "Commonly used data type DO NOT INCLUDE:",
         answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
         correctAnswer: "2"
     },
     {
-        // question 1
+        // 2nd question
         question: "The condition in an if / else statement is enclosed within ____.",
-        answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
         correctAnswer: "1"
     },
     {
-        // question 2
+        // 3rd question
         question: "Arrays in Javascript can be used to store ____.",
         answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
         correctAnswer: "3"
     },
     {
-        // question 3
+        // 4th question
         question: "String values must be enclosed within ____ when being assigned to variables.",
         answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
         correctAnswer: "2"
     },
     {
-        // question 4
+        // 4th question
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
         correctAnswer: "3"
